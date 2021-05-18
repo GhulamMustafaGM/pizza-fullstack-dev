289888ba91f5ada57da5c66e3176f96863265452
Ghulam Mustafa <mustafaji@gmail.com>
2021-05-18 21:44:12 +0200

    updated screens home

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,42 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllPizzas } from "../actions/pizzaActions";
-import Error from "../components/Error";
-import Filter from "../components/Filter";
-import Loading from "../components/Loading";
-import Pizza from "../components/Pizza";
+import React from 'react'
+import pizzas from '../pizzasdata'
+import Pizza from '../components/Pizza'
+
 export default function Homescreen() {
-    const dispatch = useDispatch();
-
-    const pizzasstate = useSelector((state) => state.getAllPizzasReducer);
-
-    const { pizzas, error, loading } = pizzasstate;
-
-    useEffect(() => {
-        dispatch(getAllPizzas());
-    }, []);
-
     return (
         <div>
-            <Filter />
-            <div className="row justify-content-center">
+            <div className="row">
+                {pizzas.map(pizza => {
+                    return <div className="col-md-4">
 
-                {loading ? (
-                    <Loading />
-                ) : error ? (
-                    <Error error='Something went wrong' />
-                ) : (
-                    pizzas.map((pizza) => {
-                        return (
-                            <div className="col-md-3 m-3" key={pizza._id}>
-                                <div>
-                                    <Pizza pizza={pizza} />
-                                </div>
-                            </div>
-                        );
-                    })
-                )}
+                        <div>
+                            <Pizza pizza={pizza} />
+                        </div>
+                    </div>
+                })}
             </div>
         </div>
-    );
+    )
 }
